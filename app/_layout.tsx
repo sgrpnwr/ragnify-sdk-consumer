1457e47ed4029c18093ad800c1ff1096e7320ed2
Sagar Panwar <85165483+sgrpnwr@users.noreply.github.com>
2026-02-19 09:23:48 +0530

    Update RootLayout with new Ragnify API credentials and base URL

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,14 +5,14 @@ import React from "react";
 export default function RootLayout() {
   return (
     <SapientAuthProvider
-      baseUrl="http://ragnifyapi.webhop.me"
-      apiKey="ba81ccfa50ceb0d68fb32eccd594cb24a4779334270642f9bcfbcbb92755c448"
-      organisationMetadata={{
-        companyName: "Bilano",
-        companyMotto: "Knowledge is power",
-        madeBy: "Sagar Panwar",
-        companyLogo: require("../assets/images/react-logo.png"),
-      }}
+      baseUrl="https://ragnifyapi.sgrpnwr.com"
+      apiKey="ea49539143faa8839e5a5ad21b30cb6086f209962a4c073345a7da67348a8839"
+      // organisationMetadata={{
+      //   companyName: "Bilano",
+      //   companyMotto: "Knowledge is power",
+      //   madeBy: "Sagar Panwar",
+      //   companyLogo: require("../assets/images/react-logo.png"),
+      // }}
     >
       <Stack screenOptions={{ headerShown: false }} />
     </SapientAuthProvider>
