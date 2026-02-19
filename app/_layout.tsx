896e01cbddba30cbda3ee8d10b46d26395b8a4e4
Sagar Panwar <85165483+sgrpnwr@users.noreply.github.com>
2026-02-19 11:41:57 +0530

    Update RootLayout with Ragnify company information and logo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,12 +7,12 @@ export default function RootLayout() {
     <SapientAuthProvider
       baseUrl="https://ragnifyapi.sgrpnwr.com"
       apiKey="ea49539143faa8839e5a5ad21b30cb6086f209962a4c073345a7da67348a8839"
-      // organisationMetadata={{
-      //   companyName: "Bilano",
-      //   companyMotto: "Knowledge is power",
-      //   madeBy: "Sagar Panwar",
-      //   companyLogo: require("../assets/images/react-logo.png"),
-      // }}
+      organisationMetadata={{
+        companyName: "Ragnify",
+        companyMotto: "Knowledge at your fingertips",
+        madeBy: "Sagar Panwar",
+        companyLogo: require("../assets/images/logo.png"),
+      }}
     >
       <Stack screenOptions={{ headerShown: false }} />
     </SapientAuthProvider>
